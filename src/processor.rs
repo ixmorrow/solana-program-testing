9368c64788c8504a527f2c5caf0b0d4937b7a5bb
ixmorrow <ivanmorrow14@gmail.com>
2022-07-19 01:05:09 -0500

    refactor

--- src/processor.rs
+++ src/processor.rs
@@ -30,10 +30,18 @@ fn process_instruction(
     let test_acct = next_account_info(account_info_iter)?;
     let system_program = next_account_info(account_info_iter)?;
 
+    msg!("Initializer pubkey: {}", initializer.key);
+    if !initializer.is_signer {
+        msg!("Account is missing signature: {}", initializer.key);
+        return Err(ProgramError::MissingRequiredSignature)
+    }
+
     let (pda, bump_seed) = Pubkey::find_program_address(
         &[initializer.key.as_ref()],
         program_id,
     );
+
+    msg!("PDA pubkey: {}", pda);
     if pda != *test_acct.key {
         msg!("Invalid seeds for PDA");
         return Err(ProgramError::InvalidArgument);
@@ -63,15 +71,59 @@ fn process_instruction(
         ]],
     )?;
 
-    msg!("Initializer pubkey: {}", initializer.key);
-    if !initializer.is_signer {
-        msg!("Account is missing signature: {}", initializer.key);
-        return Err(ProgramError::MissingRequiredSignature)
-    }
-
     let mut test_data = try_from_slice_unchecked::<TestState>(&test_acct.data.borrow()).unwrap();
     test_data.num = 1;
+    msg!("PDA data: {}", test_data.num);
 
     test_data.serialize(&mut &mut test_acct.data.borrow_mut()[..])?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use {
+        super::*,
+        assert_matches::*,
+        solana_program::{
+            instruction::{AccountMeta, Instruction},
+            system_program
+        },
+        solana_program_test::*,
+        solana_sdk::{signature::Signer, transaction::Transaction},
+    };
+
+    #[tokio::test]
+    async fn it_works() {
+        let program_id = Pubkey::new_unique();
+
+        let (mut banks_client, payer, recent_blockhash) = ProgramTest::new(
+            "adder",
+            program_id,
+            processor!(process_instruction),
+        )
+        .start()
+        .await;
+
+        let (pda, _bump_seed) = Pubkey::find_program_address(
+            &[payer.pubkey().as_ref()],
+            &program_id,
+        );
+
+        let mut transaction = Transaction::new_with_payer(
+            &[Instruction {
+                program_id,
+                accounts: vec![
+                    AccountMeta::new(payer.pubkey(), true),
+                    AccountMeta::new(pda, false),
+                    AccountMeta::new_readonly(system_program::id(), false)
+                ],
+                data: vec![1, 2, 3],
+            }],
+            Some(&payer.pubkey()),
+        );
+        transaction.sign(&[&payer], recent_blockhash);
+
+
+        assert_matches!(banks_client.process_transaction(transaction).await, Ok(_));
+    }
 }
